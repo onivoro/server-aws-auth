fe5c102b73368792a878dd9fe408ac470d34ebd7
icedlee337 <lee.e.norris@gmail.com>
2024-10-25 22:25:13 -0500

    chore: bump deps; mod tsconfig

--- src/lib/guards/auth.guard.ts
+++ src/lib/guards/auth.guard.ts
@@ -1,12 +1,11 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
 import { accessTokenKey } from '../constants/access-token-key.constant';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ) {
     const request = context.switchToHttp().getRequest();
     const hasToken = !!(request[accessTokenKey]);
     if(!hasToken) {
